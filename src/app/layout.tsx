c325d6f378408f30c29bded348fd56766d1bdce5
Mohit Gondaliya <mohitgondaliya121@gmail.com>
2024-02-17 10:28:56 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-providers";
+
+const font = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Plura",
+  description: "All in one Agency solution",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/sidebar/menu-options.tsx
@@ -0,0 +1,50 @@
+import {
+  AgencySidebarOption,
+  SubAccount,
+  SubAccountSidebarOption,
+} from "@prisma/client";
+import React, { useEffect, useMemo, useState } from "react";
+import { Sheet, SheetTrigger } from "../ui/sheet";
+
+type Props = {
+  defaultOpen?: boolean;
+  subAccounts: SubAccount[];
+  sidebarOpt: AgencySidebarOption[] | SubAccountSidebarOption[];
+  sidebarLogo: string;
+  details: any;
+  user: any;
+  id: string;
+};
+
+const MenuOptions = ({
+  defaultOpen,
+  subAccounts,
+  sidebarOpt,
+  sidebarLogo,
+  details,
+  user,
+  id,
+}: Props) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  const openState = useMemo(
+    () => (defaultOpen ? { open: true } : {}),
+    [defaultOpen]
+  );
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return;
+  return (
+    <Sheet modal={false} {...openState}>
+      <SheetTrigger
+        asChild
+        className="absolute left-4 top-4 z-[100] md:!hidden flex"
+      ></SheetTrigger>
+    </Sheet>
+  );
+};
+
+export default MenuOptions;
